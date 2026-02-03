dbe2082df6dd34e36060585f0a42dab4eac2c11c
singhaviral365 <singhaviral365@gmail.com>
2026-02-03 12:19:20 +0000

    Deleted application and service

--- backend/src/main/java/com/moneytransfersystem/application/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-// src/main/java/com/moneytransfersystem/application/AccountRepository.java
-package com.moneytransfersystem.application;
-
-import com.moneytransfersystem.domain.entities.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, String> {
-}
