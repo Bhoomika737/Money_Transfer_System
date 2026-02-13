9f85b3ee0465911e1719e89eee8969da0ba78e0b
singhaviral365 <144157728+singhaviral365@users.noreply.github.com>
2026-02-13 05:31:56 +0000

    Delete money-transfer-system directory

--- money-transfer-system/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { Login } from './components/login/login';
-import { Dashboard } from './components/dashboard/dashboard';
-
-export const routes: Routes = [
-  { path: '', component: Login },
-  { path: 'dashboard', component: Dashboard },
-  { path: '**', redirectTo: '' }
-];
